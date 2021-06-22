393349092bb95a581310886d4a3342d1da989e65
ninisha1 <86066747+ninisha1@users.noreply.github.com>
2021-06-22 11:52:08 +0530

    Create no.py

---
+++ no.py
@@ -0,0 +1,13 @@
+list1=[12,-7,5,64,-14]
+print(list1)
+for num in list1:
+    if num >= 0:
+        print(num,end=" ")
+        
+print("\n")
+
+list2=[12,14,-95,3]
+print(list2)
+for num in list2:
+    if num >= 0:
+        print(num,end=" ")
